51f9d1eacd2a3c99cf9330ec8dfe184faf9b2fcb
Yui <dev@sleepyyui.com>
2023-03-01 20:26:41 +0100

    Update logger.js

--- bot/handlers/logger.js
+++ bot/handlers/logger.js
@@ -1,27 +1,38 @@
 const beautify = require('beautify.log').default;
 
 function formatText(text) {
-    return text.replace("", "");
+  const now = new Date();
+  const day = now.getDate().toString().padStart(2, '0');
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const year = now.getFullYear().toString();
+  const hour = now.getHours().toString().padStart(2, '0');
+  const minute = now.getMinutes().toString().padStart(2, '0');
+  const second = now.getSeconds().toString().padStart(2, '0');
+  const formattedDate = `${day}.${month}.${year} ${hour}:${minute}:${second}`;
+
+  return text.replace('TIMESTAMP', formattedDate);
 }
+
+
 
 // beautify is used to colorize console logs (https://github.com/lucasgdb/beautify.log)
 // *****************************************
 // ==========================================
 module.exports = {
     info(text) {
-        const cprefix = '{fgGreen}[BOTNAME - INFO] {reset}';
+        const cprefix = '[TIMESTAMP] {fgGreen}[INFO] {reset}';
         const cformattedText = formatText(cprefix + text)
         beautify.log(cformattedText);
     },
 
     chunkmessage(text) {
-        const cprefix = '{fgBlue}[MODULE - INFO] {reset}{fgMagenta}';
+        const cprefix = '[TIMESTAMP] {fgBlue}[INFO] {reset}{fgMagenta}';
         const cformattedText = formatText(cprefix + text)
         beautify.log(cformattedText);
     },
 
     warn(text, warn) {
-        const cprefix = '{fgYellow}[BOTNAME - WARN] {reset}';
+        const cprefix = '[TIMESTAMP] {fgYellow}[WARN] {reset}';
         if (warn == null) {
             warn = text
             text = ""
@@ -31,7 +42,7 @@ module.exports = {
     },
 
     error(text, err) {
-        const cprefix = '{fgRed}[BOTNAME - ERROR] {reset}';
+        const cprefix = '[TIMESTAMP] {fgRed}[ERROR] {reset}';
         if (err === "") {
             err = "No error-message provided"
         }
@@ -44,14 +55,14 @@ module.exports = {
     },
 
     super_error(text, err) {
-        const cprefix = '{fgRed}{bright}[BOTNAME - SUPER ERROR] {bright}';
+        const cprefix = '[TIMESTAMP] {fgRed}{bright}[FATAL ERROR] {bright}';
         const cformattedText = formatText(`${cprefix + text}\n${err}`)
         beautify.log(cformattedText);
     },
 
     command(ctx) {
         if (commands) {
-            const cprefix = '{fgMagenta}[BOTNAME - COMMAND] {reset}';
+            const cprefix = '[TIMESTAMP] {fgMagenta}[COMMAND] {reset}';
             // eslint-disable-next-line max-len
             const cformattedText = formatText(`${cprefix + `${ctx.author.tag} (${ctx.author.id}) used the command ${ctx.command.name} in ${ctx.guild.name} (${ctx.guild.id}) in channel ${ctx.channel.name} (${ctx.channel.id})`}`)
             beautify.log(cformattedText);
